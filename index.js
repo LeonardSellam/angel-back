e9915cb3f8fa5498a9c1bebc7dcbc03c39f0498c
Leonard Sellam <leonard@epigenelabs.com>
2021-12-20 22:09:35 +0100

    use zoom api with JWT app

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const moment = require('moment')
+const request = require('request');
+
+var bodyParser = require('body-parser')
 const app = express()
 const port = 5000
 const cors = require('cors');
 
+const jwt_token;
 
 
 app.use(cors())
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json())
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -15,13 +28,42 @@ app.post('/events', (req, res) => {
     res.send('Hello World!')
   })
 
-app.get('/event', (req, res) => {
-console.log(req)
-res.send(JSON.stringify({ a: 1 }))
+app.post('/event', (req, res) => {
+    const startTime = moment(req.body.start);
+    const end = moment(req.body.end);
+    var duration = moment.duration(end.diff(startTime));
+
+    var options = {
+        'method': 'POST',
+        'url': 'https://api.zoom.us/v2/users/sellam.leonard@gmail.com/meetings',
+        'headers': {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${jwt_token}`,
+        },
+        body: JSON.stringify({
+            "topic": req.body.val,
+            "type":"2",
+            "duration": duration.asMinutes(),
+            "start_time":"2020-09-16T11:00:00",
+            "timezone":"Europe/Paris",
+            "password":"123456",
+            "agenda":"AGENDA"
+        })
+      
+      };
+      request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        res.send(response.body.id)
+      });
 })
+
+
+
   
   
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
